dd312515c25f21031a7a2177a08e81f38ff8d835
Daniel-Giathi <giathidaniel252@gmail.com>
2024-03-09 21:34:40 +0300

    refactor

--- my-react-app/src/pages/Charts/Line.jsx
+++ my-react-app/src/pages/Charts/Line.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from 'react';
 
+import { ChartsHeader, LineChart } from '../../components';
 const line = () => {
   return (
-    <div>line</div>
+    <div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
+      <Header category='Chart' title="Inflation Rate" />
+      <div className='w-4'>
+        <LineChart />
+      </div>
+    </div>
   )
 }
 
