80a35503737e4dc5df929b74027042b06e227e6b
Daniel-Giathi <giathidaniel252@gmail.com>
2024-03-17 09:38:44 +0300

    food-list-component

--- FoodWebsite/fullstack-foodwebsite/src/context/StoreContext.jsx
+++ FoodWebsite/fullstack-foodwebsite/src/context/StoreContext.jsx
@@ -1,11 +1,37 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import { food_list } from "../assets/assets";
 
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState({});
+
+  const addToCart = (itemId) => {
+    // Check whether the item is added for the first time in the cart
+    if (!cartItems[itemId]) {
+      setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
+    }
+    // adds the number of quantity of the items already in the cart by [itemId]
+    else {
+      setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId + 1] }));
+    }
+  };
+
+  // removes the food entry (decrease the food value by 1)
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  };
+
+  useEffect(() => {
+    console.log(itemId);
+  }, [cartItems]);
+
   const contextValue = {
     food_list,
+    cartItems,
+    setCartItems,
+    addToCart,
+    removeFromCart,
   };
   return (
     <StoreContext.Provider value={contextValue}>
