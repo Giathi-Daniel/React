876b0246cf5111dd68bdb1b43af46a8df39148de
Daniel-Giathi <giathidaniel252@gmail.com>
2024-03-17 08:47:33 +0300

    food-list styling

--- FoodWebsite/fullstack-foodwebsite/src/context/StoreContext.jsx
+++ FoodWebsite/fullstack-foodwebsite/src/context/StoreContext.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { createContext } from "react";
+import { food_list } from "../assets/assets";
 
-const StoreContext = () => {
+export const StoreContext = createContext(null);
+
+const StoreContextProvider = (props) => {
+  const contextValue = {
+    food_list,
+  };
   return (
-    <div>StoreContext</div>
-  )
-}
+    <StoreContext.Provider value={contextValue}>
+      {props.children}
+    </StoreContext.Provider>
+  );
+};
 
-export default StoreContext
+export default StoreContextProvider;
