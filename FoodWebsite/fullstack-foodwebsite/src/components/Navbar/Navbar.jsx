7514d11116e6704de8dc0eb55ab7afd5f4506f60
Daniel-Giathi <giathidaniel252@gmail.com>
2024-03-18 18:06:18 +0300

    link-functionalities

--- FoodWebsite/fullstack-foodwebsite/src/components/Navbar/Navbar.jsx
+++ FoodWebsite/fullstack-foodwebsite/src/components/Navbar/Navbar.jsx
@@ -1,19 +1,43 @@
-import React, { useState } from 'react'
-import './Navbar.css'
-import { assets } from '../../assets/assets'
+import React, { useState } from "react";
+import "./Navbar.css";
+import { assets } from "../../assets/assets";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-
   const [menu, setMenu] = useState("home");
 
   return (
     <div className="navbar">
       <img src={assets.logo} className="logo" />
       <ul className="navbar-menu">
-        <li onClick={()=> setMenu("home")} className={menu === "home"?"active":""}>home</li>
-        <li onClick={()=> setMenu("menu")} className={menu === "menu"?"active":""}>menu</li>
-        <li onClick={()=> setMenu("mobile-app")} className={menu === "mobile"?"active":""}>mobile-app</li>
-        <li onClick={()=> setMenu("contact-us")} className={menu === "contact-us"?"active":""}>contact us</li>
+        <Link
+          to="/"
+          onClick={() => setMenu("home")}
+          className={menu === "home" ? "active" : ""}
+        >
+          home
+        </Link>
+        <a
+          href="#explore-menu"
+          onClick={() => setMenu("menu")}
+          className={menu === "menu" ? "active" : ""}
+        >
+          menu
+        </a>
+        <a
+          href="#app-download"
+          onClick={() => setMenu("mobile-app")}
+          className={menu === "mobile" ? "active" : ""}
+        >
+          mobile-app
+        </a>
+        <a
+          href="#footer"
+          onClick={() => setMenu("contact-us")}
+          className={menu === "contact-us" ? "active" : ""}
+        >
+          contact us
+        </a>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
@@ -24,7 +48,7 @@ const Navbar = () => {
         <button>sign in</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
