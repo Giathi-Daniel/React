01609ff1cf183f0b1fc998fead44687eb1a95c42
Daniel-Giathi <giathidaniel252@gmail.com>
2024-04-11 18:24:42 +0300

    refactor

--- React-App/src/Components/Navbar.jsx
+++ React-App/src/Components/Navbar.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+// import logo from "./assets/react.svg";
 
 function NavBar() {
-  return <h1>Navbar Goes here!</h1>;
+  return (
+    <header>
+      {/* <img src={logo} alt="logo" /> */}
+      <h4 className="logo">ReactFacts</h4>
+      <h3>React course - Project1</h3>
+    </header>
+  );
 }
 
 export default NavBar;
