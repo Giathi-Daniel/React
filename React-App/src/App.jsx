7e6b95bac3124350f30b1c7b6027088d7db78839
Daniel-Giathi <giathidaniel252@gmail.com>
2024-04-12 17:14:21 +0300

    Airbnb Clone

--- React-App/src/App.jsx
+++ React-App/src/App.jsx
@@ -1,30 +1,26 @@
 import React from "react";
-import Jokes from "./Components/Jokes";
+import Navbar from "./Components/Navbar";
+import Hero from "./Components/Hero";
+import Card from "./Components/Card";
 
-function App() {
+/*
+Challenge: Pass props to the Card component and display that data
+
+- img ("katie-zaferes.png")
+- rating ("5.0")
+- reviewCount (6)
+- country (Whatever you want)
+- title ("Life Lessons with Katie Zaferes")
+- price (136)
+
+*/
+
+export default function App() {
+  // <Hero />
   return (
-    <>
-      <Jokes
-        setup="I got my daughter a fridge for her birthday"
-        punchline="I can't wait to see her face light up when she opens it"
-      />
-      <Jokes
-        setup="How did the hacker escape the police?"
-        punchline="He just ransomware"
-      />
-      <Jokes
-        setup="Why don't pirates travel on mountain roads?"
-        punchline="Scurvy"
-      />
-      <Jokes
-        setup="Why do bees stay in the hive in the winter?"
-        punchline="Swarm"
-      />
-      <Jokes
-        setup="What's the best thing about Switzerland?"
-        punchline="I don't know, but the flag is a big plus!"
-      />
-    </>
+    <div>
+      <Navbar />
+      <Card />
+    </div>
   );
 }
-export default App;
