cbeb6d11ed39890fc3397675187e931d0d08bddf
Daniel-Giathi <giathidaniel252@gmail.com>
2024-04-12 15:04:16 +0300

    Hero Section

--- React-App/src/App.jsx
+++ React-App/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import NavBar from "./Components/Navbar";
 import MainContent from "./Components/Main";
+import Hero from "./Components/Hero";
 
 function App() {
   return (
     <div className="container">
       <NavBar />
+      <Hero />
       <MainContent />
     </div>
   );
