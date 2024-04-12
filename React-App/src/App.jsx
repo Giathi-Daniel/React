da7e7c47083f43baba53e879ca85e9e9f7ba248a
Daniel-Giathi <giathidaniel252@gmail.com>
2024-04-12 17:29:36 +0300

    reusable props

--- React-App/src/App.jsx
+++ React-App/src/App.jsx
@@ -3,24 +3,21 @@ import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
 import Card from "./Components/Card";
 
-/*
-Challenge: Pass props to the Card component and display that data
-
-- img ("katie-zaferes.png")
-- rating ("5.0")
-- reviewCount (6)
-- country (Whatever you want)
-- title ("Life Lessons with Katie Zaferes")
-- price (136)
-
-*/
-
-export default function App() {
+function App() {
   // <Hero />
   return (
     <div>
       <Navbar />
-      <Card />
+      <Card
+        img="katie-zaferes.png"
+        rating="5.0"
+        reviewCount={6}
+        country="Canada"
+        title="Life Lessons with Katie Zaferes"
+        price={136}
+      />
     </div>
   );
 }
+
+export default App;
--- React-App/src/Components/Card.jsx
+++ React-App/src/Components/Card.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 
-export default function Card() {
+function Card({ img, rating, country, reviewCount, price }) {
   return (
     <div className="card">
-      <img src="../images/katie-zaferes.png" className="card--image" />
+      <img src={`../images/${img}`} className="card--image" />
       <div className="card--stats">
         <img src="../images/star.png" className="card--star" />
-        <span>5.0</span>
-        <span className="gray">(6) • </span>
-        <span className="gray">USA</span>
+        <span>{rating}</span>
+        <span className="gray">{reviewCount} • </span>
+        <span className="gray">{country}</span>
       </div>
       <p>Life Lessons with Katie Zaferes</p>
       <p>
-        <span className="bold">From $136</span> / person
+        <span className="bold">From ${price}</span> / person
       </p>
     </div>
   );
 }
+
+export default Card;
