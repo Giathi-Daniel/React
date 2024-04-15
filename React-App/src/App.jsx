a8b0b74dbaf57a59067d9145f435b1eed37e4f74
Daniel-Giathi <giathidaniel252@gmail.com>
2024-04-15 16:43:30 +0300

    settingBadges

--- React-App/src/App.jsx
+++ React-App/src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   const cardElement = data.map((item) => {
     return (
       <Card
+        key={item.id}
         img={item.coverImg}
         rating={item.stats.rating}
         reviewCount={item.stats.reviewCount}
@@ -18,6 +19,7 @@ function App() {
       />
     );
   });
+
   return (
     <div>
       <Navbar />
--- React-App/src/Components/Card.jsx
+++ React-App/src/Components/Card.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 
 function Card({ img, rating, country, reviewCount, price }) {
+  let badgeText;
+
+  if (props.openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (props.location === "ONLINE") {
+    badgeText = "ONLINE";
+  }
+
   return (
     <div className="card">
+      {badgeText && <div className="card--badge">{badgeText}</div>}
+      {props.openSpots === 0 && <div className="card--badge">SOLD OUT</div>}
       <img src={`../images/${img}`} className="card--image" />
       <div className="card--stats">
         <img src="../images/star.png" className="card--star" />
