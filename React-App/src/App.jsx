583881948b7794648ba8abaf4b0950ca14e864db
Daniel-Giathi <giathidaniel252@gmail.com>
2024-04-15 16:24:31 +0300

    MappingThroughComponents

--- React-App/src/App.jsx
+++ React-App/src/App.jsx
@@ -2,20 +2,26 @@ import React from "react";
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
 import Card from "./Components/Card";
+import data from "./data";
 
 function App() {
   // <Hero />
+  const cardElement = data.map((item) => {
+    return (
+      <Card
+        img={item.coverImg}
+        rating={item.stats.rating}
+        reviewCount={item.stats.reviewCount}
+        country={item.location}
+        title={item.title}
+        price={item.price}
+      />
+    );
+  });
   return (
     <div>
       <Navbar />
-      <Card
-        img="katie-zaferes.png"
-        rating="5.0"
-        reviewCount={6}
-        country="Canada"
-        title="Life Lessons with Katie Zaferes"
-        price={136}
-      />
+      {cardElement}
     </div>
   );
 }
