a6aec9b27175359fabe9573cc11751897bdf2fb4
Daniel-Giathi <giathidaniel252@gmail.com>
2024-04-17 16:31:35 +0300

    refactor

---
+++ memeGenerator/src/components/meme/Meme.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Meme.css";
+
+const Meme = () => {
+  return (
+    <main>
+      <div className="form">
+        <input type="text" placeholder="Top text" className="form--input" />
+        <input type="text" placeholder="Bottom text" className="form--input" />
+        <button className="form--button">Get a new meme image 🖼</button>
+      </div>
+    </main>
+  );
+};
+
+export default Meme;
