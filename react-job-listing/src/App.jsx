c3e64dd40704a7706917067eaae80bcdbd2c0a38
Daniel-Giathi <giathidaniel252@gmail.com>
2024-06-04 12:17:38 +0300

    Add install react-router-dom component

--- react-job-listing/src/App.jsx
+++ react-job-listing/src/App.jsx
@@ -1,21 +1,26 @@
-import React from 'react'
-import Navbar from './Components/Navbar/Navbar'
-import Hero from './Components/Hero/Hero'
-import HomeCards from './Components/HomeCards/HomeCards'
-import JobsListing from './Components/Jobs/JobsListings'
-import ViewAllJobs from './Components/Jobs/viewAllJobs'
-
+import React from "react";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/Hero/Hero";
+import HomeCards from "./Components/HomeCards/HomeCards";
+import JobsListing from "./Components/Jobs/JobsListings";
+import ViewAllJobs from "./Components/Jobs/viewAllJobs";
 
 const App = () => {
   return (
     <>
-    <Navbar />
-    <Hero/>
-    <HomeCards />
-    <JobsListing />
-    <ViewAllJobs />
+      <Navbar />
+      <Hero />
+      <HomeCards />
+      <JobsListing />
+      <ViewAllJobs />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
