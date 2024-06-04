8fa6ef202f5e6b35c98ebf2a15b4c1033d0b1371
Daniel-Giathi <giathidaniel252@gmail.com>
2024-06-04 23:20:21 +0300

    Create JobPage component

--- react-job-listing/src/App.jsx
+++ react-job-listing/src/App.jsx
@@ -9,12 +9,14 @@ import Home from "./Pages/Home";
 import MainLayout from "./Layouts/MainLayout";
 import JobsPage from "./Pages/JobsPage";
 import PageNotFound from "./Pages/PageNotFound";
+import JobPage, { jobLoader } from "./Pages/JobPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<Home />} />
       <Route path="/jobs" element={<JobsPage />} />
+      <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />
       <Route path="*" element={<PageNotFound />} />
     </Route>
   )
