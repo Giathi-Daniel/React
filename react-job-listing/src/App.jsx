6594ee26657f6e80d3253fb6e46bf49e1a6da688
Daniel-Giathi <giathidaniel252@gmail.com>
2024-06-04 12:40:47 +0300

    Create JobsPage component

--- react-job-listing/src/App.jsx
+++ react-job-listing/src/App.jsx
@@ -7,11 +7,13 @@ import {
 } from "react-router-dom";
 import Home from "./Pages/Home";
 import MainLayout from "./Layout/MainLayout";
+import JobsPage from "./Pages/JobsPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<Home />} />
+      <Route path='/jobs' element={<JobsPage/>} />
     </Route>
   )
 );
